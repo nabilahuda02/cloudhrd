34512a6fa03832a50ba5d134b446fd5153cbe642
Zulfa Juniadi Zulkifli <zulfajuniadi@gmail.com>
2014-12-05 17:56:34 +0800

    changed menu to use blade syntax {{}}

--- web/app/app/views/admin/users/menu.blade.php
+++ web/app/app/views/admin/users/menu.blade.php
@@ -1,7 +1,8 @@
 <div class="pull-right">
     <div class="btn-group">
-        <a href="<?php echo action('AdminUserController@index'); ?>" class="btn btn-default {{Helper::isRouteAction('AdminUserController@index')}}">View Users</a>
-        <a href="<?php echo action('AdminUserController@create'); ?>" class="btn btn-default {{Helper::isRouteAction('AdminUserController@create')}}">Create User</a>
-        <a href="<?php echo action('AdminUserController@getManageTemplate'); ?>" class="btn btn-default {{Helper::isRouteAction('AdminUserController@getManageTemplate')}}">Manage User Template</a>
+        <a href="{{action('AdminUserController@index')}}" class="btn btn-default {{Helper::isRouteAction('AdminUserController@index')}}">View Users</a>
+        <a href="{{action('AdminUserController@create')}}" class="btn btn-default {{Helper::isRouteAction('AdminUserController@create')}}">Create User</a>
+        <a href="{{action('AdminUserController@getManageTemplate')}}" class="btn btn-default {{Helper::isRouteAction('AdminUserController@getManageTemplate')}}">Manage User Template</a>
+        <a href="{{action('AdminUserController@getImportUsers')}}" class="btn btn-default {{Helper::isRouteAction('AdminUserController@getImportUsers')}}">Import Users</a>
     </div>
 </div>
