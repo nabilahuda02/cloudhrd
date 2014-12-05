a07407ff7a9175273b35eaa0b8d29fd74468ff4d
Zulfa Juniadi Zulkifli <zulfajuniadi@gmail.com>
2014-12-05 17:56:58 +0800

    Notifications are now aligned to the left

--- web/app/app/views/html/notifications.blade.php
+++ web/app/app/views/html/notifications.blade.php
@@ -1,12 +1,12 @@
 @if(Session::get('NotifySuccess'))
-  <div class="text-center alert alert-success">{{ Session::get('NotifySuccess') }}</div>
+  <div class="alert alert-success">{{ Session::get('NotifySuccess') }}</div>
 @endif
 @if(Session::get('NotifyWarning'))
-  <div class="text-center alert alert-warning">{{ Session::get('NotifyWarning') }}</div>
+  <div class="alert alert-warning">{{ Session::get('NotifyWarning') }}</div>
 @endif
 @if(Session::get('NotifyDanger'))
-  <div class="text-center alert alert-danger">{{ Session::get('NotifyDanger') }}</div>
+  <div class="alert alert-danger">{{ Session::get('NotifyDanger') }}</div>
 @endif
 @if(Session::get('NotifyInfo'))
-  <div class="text-center alert alert-info">{{ Session::get('NotifyInfo') }}</div>
+  <div class="alert alert-info">{{ Session::get('NotifyInfo') }}</div>
 @endif
