ddd3b17c1a448bfe67b72a895739c315c0c12876
Zulfa Juniadi bin Zulkifli <zulfajuniadi@gmail.com>
2016-03-21 11:31:25 +0800

    Add fields to user profile

--- web/app/app/models/UserProfile.php
+++ web/app/app/models/UserProfile.php
@@ -3,34 +3,42 @@
 class UserProfile extends Eloquent
 {
 
-  public static $validation_rules = [];
+    public static $validation_rules = [];
 
-  protected $fillable = [
-    'user_id',
-    'first_name',
-    'last_name',
-    'address',
-    'user_field_00',
-    'user_field_01',
-    'user_field_02',
-    'user_field_03',
-    'user_field_04',
-    'user_field_05',
-    'user_field_06',
-    'user_field_07',
-    'user_field_08',
-    'user_field_09',
-  ];
+    protected $fillable = [
+        'user_id',
+        'first_name',
+        'last_name',
+        'address',
+        'user_field_00',
+        'user_field_01',
+        'user_field_02',
+        'user_field_03',
+        'user_field_04',
+        'user_field_05',
+        'user_field_06',
+        'user_field_07',
+        'user_field_08',
+        'user_field_09',
+        'bank_name',
+        'bank_account',
+        'kwsp_account',
+        'kwsp_contribution',
+        'lhdn_account',
+        'pcb_contribution',
+        'socso_account',
+        'socso_contribution',
+        'salary',
+    ];
 
+    public function user()
+    {
+        return $this->belongsTo('User');
+    }
 
-  public function user()
-  {
-    return $this->belongsTo('User');
-  }
-
-  public function userName()
-  {
-    return $this->first_name . ' ' . $this->last_name; 
-  }
+    public function userName()
+    {
+        return $this->first_name . ' ' . $this->last_name;
+    }
 
 }
--- web/app/app/views/admin/users/form.blade.php
+++ web/app/app/views/admin/users/form.blade.php
@@ -1,38 +1,95 @@
 {{ Former::email('email')
-    ->required() }}
-
+->required() }}
 {{ Former::text('first_name')
-    ->label('First Name')
-    ->required()
-    ->value(@$currentuser->profile->first_name) }}
-
+->label('First Name')
+->required()
+->value(@$currentuser->profile->first_name) }}
 {{ Former::text('last_name')
-    ->label('Last Name')
-    ->value(@$currentuser->profile->last_name) }}
-
+->label('Last Name')
+->value(@$currentuser->profile->last_name) }}
 {{ Former::textarea('address')
-    ->label('Address')
-    ->value(@$currentuser->profile->address) }}
-
-{{ Former::select('unit_id')
-    ->label('Unit')
-    ->options(UserUnit::all()->lists('name', 'id'))
-    ->required() }}
-
-<?php $i = 0; ?>
+->label('Address')
+->value(@$currentuser->profile->address) }}
+<div class="row">
+    <div class="col-md-6">
+        {{ Former::select('unit_id')
+        ->label('Unit')
+        ->options(UserUnit::all()->lists('name', 'id'))
+        ->required() }}
+    </div>
+    <div class="col-md-6">
+        {{ Former::text('salary')
+        ->value(@$currentuser->profile->salary)
+        ->label('Salary')
+        ->required() }}
+    </div>
+    <div class="col-md-6">
+        {{ Former::text('bank_name')
+        ->value(@$currentuser->profile->bank_name)
+        ->label('Bank Name')
+        ->required() }}
+    </div>
+    <div class="col-md-6">
+        {{ Former::text('bank_account')
+        ->value(@$currentuser->profile->bank_account)
+        ->label('Bank Account Number')
+        ->required() }}
+    </div>
+    <div class="col-md-6">
+        {{ Former::text('kwsp_account')
+        ->value(@$currentuser->profile->kwsp_account)
+        ->label('KWSP Account Number') }}
+    </div>
+    <div class="col-md-6">
+        {{ Former::text('kwsp_contribution')
+        ->value(@$currentuser->profile->kwsp_contribution)
+        ->label('KWSP Contribution Percentage') }}
+    </div>
+    <div class="col-md-6">
+        {{ Former::text('socso_account')
+        ->value(@$currentuser->profile->socso_account)
+        ->label('SOCSO Account Number') }}
+    </div>
+    <div class="col-md-6">
+        {{ Former::text('socso_contribution')
+        ->value(@$currentuser->profile->socso_contribution)
+        ->label('SOCSO Contribution Percentage') }}
+    </div>
+    <div class="col-md-6">
+        {{ Former::text('lhdn_account')
+        ->value(@$currentuser->profile->lhdn_account)
+        ->label('PCB Account Number') }}
+    </div>
+    <div class="col-md-6">
+        {{ Former::text('pcb_contribution')
+        ->value(@$currentuser->profile->pcb_contribution)
+        ->label('PCB Contrubution') }}
+    </div>
+</div>
+<!--
+'bank_name',
+'bank_account',
+'kwsp_account',
+'kwsp_contribution',
+'lhdn_account',
+'pcb_contribution',
+'socso_account',
+'socso_contribution',
+'salary',
+-->
+<?php $i = 0;?>
 @foreach (app()->user_locale->profile_custom_fields as $key => $value)
-  @if($value)
-    {{ Former::text('user_field_0' . $i)
-          ->label($value)
-          ->value(@$currentuser->profile->{'user_field_0' . $i}) }}
-  @endif
-  <?php $i++; ?>
+@if($value)
+{{ Former::text('user_field_0' . $i)
+->label($value)
+->value(@$currentuser->profile->{'user_field_0' . $i}) }}
+@endif
+<?php $i++;?>
 @endforeach
-
 {{Former::radios('is_admin')
-  ->radios([
-      'True' => array('value' => '1'),
-      'False' => array('value' => '0'),
-    ])
-  ->required()
-  ->stacked() }}
+->radios([
+'True' => array('value' => '1'),
+'False' => array('value' => '0'),
+])
+->required()
+->stacked() }}
