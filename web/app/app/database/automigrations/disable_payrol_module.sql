3ed26c88b16596221ac5b21d0eb4c960dae100d5
Zulfa Juniadi bin Zulkifli <zulfajuniadi@gmail.com>
2016-03-21 10:45:51 +0800

    Add enabled flag inside modules table

---
+++ web/app/app/database/automigrations/disable_payrol_module.sql
@@ -0,0 +1,3 @@
+INSERT INTO `modules` (`id`, `name`, `verifier`, `approver`, `has_config`, `enabled`, `created_at`, `updated_at`)
+VALUES
+    (5, 'Payrol', -2, -1, 1, 0, '2016-03-21 10:40:21', '2016-03-21 10:40:21');
