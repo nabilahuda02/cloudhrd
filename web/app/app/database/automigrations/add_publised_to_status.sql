b20c38997a01191d7d5ea2f472721cf7b7096db8
Zulfa Juniadi <zulfajuniadi@gmail.com>
2016-05-10 01:07:49 +0800

    Remove old migrations

--- web/app/app/database/automigrations/add_publised_to_status.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `status` (`id`, `name`, `overide_name`, `created_at`, `updated_at`, `deleted_at`)
-VALUES
-    (7, 'Published', NULL, '2014-11-06 12:06:53', '2014-11-06 12:06:53', NULL);
