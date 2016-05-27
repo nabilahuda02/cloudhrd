d353fbc0c53d99c8b9bd8bc2fd1f7fe0b3d82817
Zulfa Juniadi <zulfajuniadi@gmail.com>
2016-05-28 02:58:48 +0800

    Hide org chart

--- web/app/app/views/admin/units/menu.blade.php
+++ web/app/app/views/admin/units/menu.blade.php
@@ -2,6 +2,6 @@
     <div class="btn-group">
         <a href="<?php echo action('AdminUnitController@index'); ?>" class="btn btn-default {{Helper::isRouteAction('AdminUnitController@index')}}">View Units</a>
         <a href="<?php echo action('AdminUnitController@create'); ?>" class="btn btn-default {{Helper::isRouteAction('AdminUnitController@create')}}">Create New Unit</a>
-        <a href="<?php echo action('AdminUnitController@getChart'); ?>" class="btn btn-default {{Helper::isRouteAction('AdminUnitController@getChart')}}">View Org Chart</a>
+        <!-- <a href="<?php echo action('AdminUnitController@getChart'); ?>" class="btn btn-default {{Helper::isRouteAction('AdminUnitController@getChart')}}">View Org Chart</a> -->
     </div>
 </div>
