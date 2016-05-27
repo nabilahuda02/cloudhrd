32bbf75b4aacfe1a5fde1e56fb264dd2f1f87ed4
Zulfa Juniadi <zulfajuniadi@gmail.com>
2016-05-28 03:56:43 +0800

    Fix menu for mobile

--- web/app/app/views/medicals/menu.blade.php
+++ web/app/app/views/medicals/menu.blade.php
@@ -1,7 +1,7 @@
 @if($user->administers(MedicalClaim__Main::$moduleId))
 <div class="btn-group pull-right">
     @if(Route::currentRouteAction() === 'MedicalController@index')
-        <a href="<?php echo url('medical/create'); ?>" class="btn btn-primary">Create Claim</a>
+        <a href="<?php echo url('medical/create'); ?>" class="btn btn-primary">New Claim</a>
     @else
         <a href="<?php echo url('medical'); ?>" class="btn btn-primary">List Claims</a>
     @endif
@@ -12,7 +12,7 @@
     <ul class="dropdown-menu dropdown-menu-right text-left" role="menu">
         <li><a href="javascript:;"  onclick="HelpFile.show('ADMIN_MANAGE_MC.md', 'Medical Claims Administration Help')">Medical Claims Administration Help</a></li>
         @if(Route::currentRouteAction() !== 'MedicalController@index' && Route::currentRouteAction() !== 'MedicalController@create')
-            <li><a href="<?php echo url('medical/create'); ?>">Create Claim</a></li>
+            <li><a href="<?php echo url('medical/create'); ?>">New Claim</a></li>
         @endif
         <li><a href="<?php echo url('medical/admin/types', $parameters = array(), $secure = null); ?>"> Manage Types</a></li>
         <li><a href="<?php echo url('medical/admin/entitlements', $parameters = array(), $secure = null); ?>"> Manage User Entitlement</a></li>
@@ -24,7 +24,7 @@
         @if(Route::currentRouteAction() !== 'MedicalController@index')
             <a href="<?php echo url('medical'); ?>" class="btn btn-primary">List Claims</a>
         @endif
-        <a href="<?php echo url('medical/create'); ?>" class="btn btn-primary">Create Claim</a>
+        <a href="<?php echo url('medical/create'); ?>" class="btn btn-primary">New Claim</a>
     </div>
 
 @endif
