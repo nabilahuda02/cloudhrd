93fd965cdab35feba0ee5d3e96c0faad542b70ec
Zulfa Juniadi bin Zulkifli <zulfajuniadi@gmail.com>
2016-09-01 11:15:51 +0800

    Fix General Claims inner structure

--- web/app/app/views/generalclaims/edit.blade.php
+++ web/app/app/views/generalclaims/edit.blade.php
@@ -1,132 +1,154 @@
 @extends('layouts.module')
 @section('content')
-<div class="col-md-12">
-
-    @include('html.notifications')
-    @include('generalclaims.header')
-    <div class="col-md-12">
-        <div >
-            <h4>Claim Application Form
-        </div>
-        <div style="padding:15px;">
-            {{ Former::horizontal_open(action('GeneralClaimsController@update', $claim->id))
-            -> id('MyForm')
-            -> rules(['name' => 'required'])
-            -> method('POST') }}
-            {{ Former::hidden('_method', 'PUT') }}
-            {{ Former::text('ref')
-            -> label('Reference')
-            -> value($claim->ref)
-            -> readonly()
-            -> disabled() }}
-            {{ Former::text('user_name')
-            -> label('Employee')
-            -> value(User::fullName($claim->user_id))
-            -> readonly() }}
-            {{ Former::text('status_name')
-            -> label('Status')
-            -> value($claim->status->name)
-            -> readonly()
-            -> disabled() }}
-            {{ Former::populate($claim) }}
-            {{ Former::text('title')
-            -> required() }}
-            <div class="form-group">
-                <div class="col-lg-offset-2 col-sm-offset-4 col-lg-10 col-sm-8">
-                    <table class="table table-bordered table-hover">
-                        <thead>
-                            <tr>
-                                <td>Receipt Date</td>
-                                <td>Receipt Number</td>
-                                <td>Type</td>
-                                <td>Description</td>
-                                <td width="150px">Subtotal</td>
-                                <td width="90px">-</td>
-                            </tr>
-                        </thead>
-                        <tbody id="targettbody">
-                            @foreach($claim->entries as $entry)
-                            <tr><td><input type="hidden" name="entries[]" value='{{json_encode($entry)}}' /> {{$entry->receipt_date}}</td><td>{{ $entry->receipt_number }}</td><td>{{$entry->type->name}}</td><td>{{$entry->description}}</td><td class="amount_col">{{$entry->amount}}</td><td><button type="button" class="removerow"><span class="glyphicon glyphicon-trash"></span></button></td></tr>
-                            @endforeach
-                        </tbody>
-                    </table>
-                    <button type="button" id="newrow" class="btn btn-secondary">
-                    <span class="glyphicon glyphicon-plus"></span>
+<section id="generalclaim">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-9">
+                <h2>
+                    @if($claim->is_paid)
+                        <span class="pull-right label label-success">Paid</span>
+                    @else
+                        <span class="pull-right label label-warning">Unpaid</span>
+                    @endif
+                    General Claims Form: {{$claim->ref}}
+                    <button class="btn btn-link help-btn" onclick="HelpFile.show('USER_GC.md', 'General Claims')">
+                        <i class="fa fa-question-circle"></i>
                     </button>
-                </div>
+                </h2>
             </div>
-            {{ Former::number('value')
-            -> readonly()
-            -> placeholder('0.00')
-            -> required() }}
-
-            {{ Asset::push('js','upload')}}
-            <div class="form-group">
-                <label for="dates" class="control-label col-lg-2 col-sm-4">Uploaded</label>
-                <div class="col-lg-10 col-sm-8">
-                    <ul class="list-inline uploaded">
-                        @foreach ($claim->uploads as $file)
-                        <li class="view_uploaded" data-url="{{$file->file_url}}">
-                            <button type="button" class="btn btn-danger remove_uploaded" data-id="{{$file->id}}">&times;</button>
-                            <a href="{{$file->file_url}}" target="_blank">{{$file->file_name}}</a>
-                        </li>
-                        @endforeach
-                    </ul>
-                </div>
+            <div class="col-xs-3 section-drop-menu" >
+                @include('generalclaims.menu')
             </div>
-            <div class="form-group">
-                <label for="dates" class="control-label col-lg-2 col-sm-4">Upload<br/>(If Any)</label>
-                <div class="col-lg-10 col-sm-8 dropzone" id="upload" data-path="generalclaim/{{$claim->upload_hash}}/{{$claim->id}}">
-                </div>
-            </div>
-            {{ Former::textarea('remarks') }}
-            <div class="form-group">
-                <div class="col-lg-offset-2 col-sm-offset-4 col-lg-10 col-sm-8">
-                    @include('generalclaims.actions-buttons')
-
-                    <input class="btn-large btn-primary btn pull-right click-once" type="submit" value="Submit">
-                </div>
-            </div>
-            {{ Former::close() }}
         </div>
     </div>
-</div>
+    <hr/>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-9">
+                @include('html.notifications')
+                {{ Former::horizontal_open(action('GeneralClaimsController@update', $claim->id))
+                    ->id('MyForm')
+                    ->rules(['name' => 'required'])
+                    ->method('POST') }}
+                {{ Former::hidden('_method', 'PUT') }}
+                {{ Former::text('ref')
+                    ->label('Reference')
+                    ->value($claim->ref)
+                    ->readonly()
+                    ->disabled() }}
+                {{ Former::text('user_name')
+                    ->label('Employee')
+                    ->value(User::fullName($claim->user_id))
+                    ->readonly() }}
+                {{ Former::text('status_name')
+                    ->label('Status')
+                    ->value($claim->status->name)
+                    ->readonly()
+                    ->disabled() }}
+                {{ Former::populate($claim) }}
+                {{ Former::text('title')
+                    ->required() }}
+                <div class="form-group">
+                    <div class="col-lg-offset-2 col-sm-offset-4 col-lg-10 col-sm-8">
+                        <table class="table table-bordered table-hover">
+                            <thead>
+                                <tr>
+                                    <td>Receipt Date</td>
+                                    <td>Receipt Number</td>
+                                    <td>Type</td>
+                                    <td>Description</td>
+                                    <td width="150px">Subtotal</td>
+                                    <td width="90px">-</td>
+                                </tr>
+                            </thead>
+                            <tbody id="targettbody">
+                                @foreach($claim->entries as $entry)
+                                <tr><td><input type="hidden" name="entries[]" value='{{json_encode($entry)}}' /> {{$entry->receipt_date}}</td><td>{{ $entry->receipt_number }}</td><td>{{$entry->type->name}}</td><td>{{$entry->description}}</td><td class="amount_col">{{$entry->amount}}</td><td><button type="button" class="removerow"><span class="glyphicon glyphicon-trash"></span></button></td></tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                        <button type="button" id="newrow" class="btn btn-secondary">
+                        <span class="glyphicon glyphicon-plus"></span>
+                        </button>
+                    </div>
+                </div>
+                {{ Former::number('value')
+                    ->readonly()
+                    ->placeholder('0.00')
+                    ->required() }}
+
+                {{ Asset::push('js','upload')}}
+                @if($claim->uploads()->count() > 0)
+                <div class="form-group">
+                    <label for="dates" class="control-label col-lg-2 col-sm-4">Uploaded</label>
+                    <div class="col-lg-10 col-sm-8">
+                        <ul class="list-inline uploaded">
+                            @foreach ($claim->uploads as $file)
+                            <li class="view_uploaded" data-url="{{$file->file_url}}">
+                                <button type="button" class="btn btn-danger remove_uploaded" data-id="{{$file->id}}">&times;</button>
+                                <a href="{{$file->file_url}}" target="_blank">{{$file->file_name}}</a>
+                            </li>
+                            @endforeach
+                        </ul>
+                    </div>
+                </div>
+                @endif
+                <div class="form-group">
+                    <label for="dates" class="control-label col-lg-2 col-sm-4">Upload<br/>(If Any)</label>
+                    <div class="col-lg-10 col-sm-8 ">
+                        <div class="dropzone" id="upload" data-path="generalclaim/{{$claim->upload_hash}}/{{$claim->id}}">
+                        </div>
+                    </div>
+                </div>
+                {{ Former::textarea('remarks') }}
+                <div class="form-group">
+                    <div class="col-lg-offset-2 col-sm-offset-4 col-lg-10 col-sm-8">
+                        @include('generalclaims.actions-buttons')
+
+                        <input class="btn-large btn-primary btn pull-right click-once" type="submit" value="Submit">
+                    </div>
+                </div>
+                {{ Former::close() }}
+            </div>
+        </div>
+    </div>
+</section>
 <template id="claimform">
 <br>
 <br>
 {{ Former::horizontal_open()
-    -> id('entryform') }}
+    ->id('entryform') }}
 {{ Former::select('claim_type_id')
-    -> id('form_type')
-    -> label('Type')
-    -> required()
-    -> options(GeneralClaim__Type::all()->lists('name', 'id'))
-    -> placeholder('Choose a claim type') }}
+    ->id('form_type')
+    ->label('Type')
+    ->required()
+    ->options(GeneralClaim__Type::all()->lists('name', 'id'))
+    ->placeholder('Choose a claim type') }}
 {{ Former::input('receipt_date')
-    -> data_type('date')
-    -> value(Helper::today_short_date())
-    -> label('Date')
-    -> required() }}
+    ->data_type('date')
+    ->value(Helper::today_short_date())
+    ->label('Date')
+    ->required() }}
 {{ Former::text('receipt_number')
-    -> label('Receipt Number')
-    -> required() }}
+    ->label('Receipt Number')
+    ->required() }}
 {{ Former::text('description')
-    -> id('form_title')
-    -> required() }}
+    ->id('form_title')
+    ->required() }}
 @foreach(GeneralClaim__Type::where('unit', '!=', '')->get() as $type)
 {{ Former::number('quantity[]')
-    -> step(1 / pow(10, app()->user_locale->decimal_places))
-    -> class('form-control quantities')
-    -> id('form_quantity_' . $type->id)
-    -> label('Quantity (' . $type->unit . ')')
-    -> data_unitprice($type->unit_price)
-    -> help($user_locale->currency_symbol . ' ' . $type->unit_price . ' / ' . $type->unit) }}
+    ->step(1 / pow(10, app()->user_locale->decimal_places))
+    ->class('form-control quantities')
+    ->id('form_quantity_' . $type->id)
+    ->label('Quantity (' . $type->unit . ')')
+    ->data_unitprice($type->unit_price)
+    ->help($user_locale->currency_symbol . ' ' . $type->unit_price . ' / ' . $type->unit) }}
 @endforeach
 {{ Former::number('amount')
-    -> step(1 / pow(10, app()->user_locale->decimal_places))
-    -> id('form_amount')
-    -> required()
-    -> readonly() }}
+    ->step(1 / pow(10, app()->user_locale->decimal_places))
+    ->id('form_amount')
+    ->required()
+    ->readonly() }}
 {{ Former::close() }}
 </template>
 <script>
@@ -147,7 +169,7 @@
         }
     };
 </script>
-{{Asset::push('js', 'app/general_claimd')}}
+{{Asset::push('js', 'general_claims')}}
 @stop
 @section('script')
     @include('generalclaims.actions-scripts')
--- web/app/app/views/generalclaims/show.blade.php
+++ web/app/app/views/generalclaims/show.blade.php
@@ -1,100 +1,117 @@
 @extends('layouts.module')
 @section('content')
-<div class="col-md-12">
-    @include('html.notifications')
-    @include('generalclaims.header')
-    <div class="col-md-12">
-        <h3>{{ $claim->is_paid ? 'Paid' : 'Unpaid' }}</h3>
-    </div>
-    <div class="col-md-12">
-        <div >
-            <h4>Claim Application Form</h4>
-            <div class="clearfix"></div>
+<section id="generalclaim">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-9">
+                <h2>
+                    @if($claim->is_paid)
+                        <span class="pull-right label label-success">Paid</span>
+                    @else
+                        <span class="pull-right label label-warning">Unpaid</span>
+                    @endif
+                    General Claims Form: {{$claim->ref}}
+                    <button class="btn btn-link help-btn" onclick="HelpFile.show('USER_GC.md', 'General Claims')">
+                        <i class="fa fa-question-circle"></i>
+                    </button>
+                </h2>
+            </div>
+            <div class="col-xs-3 section-drop-menu" >
+                @include('generalclaims.menu')
+            </div>
         </div>
-        <div style="padding:15px;">
-            {{ Former::horizontal_open(action('GeneralClaimsController@store'))
-                -> id('MyForm')
-                -> rules(['name' => 'required'])
-                -> method('POST') }}
-            {{ Former::text('created_at')
-                -> label('Created At')
-                -> value(Helper::timestamp($claim->created_at))
-                -> readonly()
-                -> disabled() }}
-            {{ Former::text('ref')
-                -> label('Reference')
-                -> value($claim->ref)
-                -> readonly()
-                -> disabled() }}
-            {{ Former::text('user_id')
-                -> label('Employee')
-                -> value(User::fullName($claim->user_id))
-                -> readonly()
-                -> disabled() }}
-            {{ Former::text('status_name')
-                -> label('Status')
-                -> value($claim->status->name)
-                -> readonly()
-                -> disabled() }}
-            {{Former::populate($claim)}}
-            {{ Former::text('title')
-                -> readonly()
-                -> disabled() }}
-            <div class="form-group">
-                <div class="col-lg-offset-2 col-sm-offset-4 col-lg-10 col-sm-8">
-                    <table class="table table-bordered table-hover">
-                        <thead>
-                            <tr>
-                                <td>Receipt Date</td>
-                                <td>Receipt Number</td>
-                                <td>Type</td>
-                                <td>Description</td>
-                                <td width="150px">Subtotal</td>
-                            </tr>
-                        </thead>
-                        <tbody id="targettbody">
-                            @foreach ($claim->entries as $entry)
-                            <tr>
-                                <td>{{ $entry->receipt_date }}</td>
-                                <td>{{ $entry->receipt_number }}</td>
-                                <td>{{ $entry->type->name }}</td>
-                                <td>{{ $entry->description }}</td>
-                                <td class="amount_col">{{ $entry->amount }}</td>
-                            </tr>
+    </div>
+    <hr/>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-9">
+                @include('html.notifications')
+                {{ Former::horizontal_open(action('GeneralClaimsController@store'))
+                    ->id('MyForm')
+                    ->rules(['name' => 'required'])
+                    ->method('POST') }}
+                {{ Former::text('created_at')
+                    ->label('Created At')
+                    ->value(Helper::timestamp($claim->created_at))
+                    ->readonly()
+                    ->disabled() }}
+                {{ Former::text('ref')
+                    ->label('Reference')
+                    ->value($claim->ref)
+                    ->readonly()
+                    ->disabled() }}
+                {{ Former::text('user_id')
+                    ->label('Employee')
+                    ->value(User::fullName($claim->user_id))
+                    ->readonly()
+                    ->disabled() }}
+                {{ Former::text('status_name')
+                    ->label('Status')
+                    ->value($claim->status->name)
+                    ->readonly()
+                    ->disabled() }}
+                {{Former::populate($claim)}}
+                {{ Former::text('title')
+                    ->readonly()
+                    ->disabled() }}
+                <div class="form-group">
+                    <div class="col-lg-offset-2 col-sm-offset-4 col-lg-10 col-sm-8">
+                        <table class="table table-bordered table-hover">
+                            <thead>
+                                <tr>
+                                    <td>Receipt Date</td>
+                                    <td>Receipt Number</td>
+                                    <td>Type</td>
+                                    <td>Description</td>
+                                    <td width="150px">Subtotal</td>
+                                </tr>
+                            </thead>
+                            <tbody id="targettbody">
+                                @foreach ($claim->entries as $entry)
+                                <tr>
+                                    <td>{{ $entry->receipt_date }}</td>
+                                    <td>{{ $entry->receipt_number }}</td>
+                                    <td>{{ $entry->type->name }}</td>
+                                    <td>{{ $entry->description }}</td>
+                                    <td class="amount_col">{{ $entry->amount }}</td>
+                                </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+                {{ Former::number('value')
+                    ->readonly()
+                    ->disabled()
+                    ->placeholder('0.00') }}
+                @if($claim->uploads()->count() > 0)
+                {{ Asset::push('js','upload')}}
+                <div class="form-group">
+                    <label for="dates" class="control-label col-lg-2 col-sm-4">Uploaded</label>
+                    <div class="col-lg-10 col-sm-8">
+                        <ul class="list-inline uploaded">
+                            @foreach ($claim->uploads as $file)
+                            <li class="view_uploaded" data-url="{{$file->file_url}}">
+                                <a href="{{$file->file_url}}" target="_blank">{{$file->file_name}}</a>
+                            </li>
                             @endforeach
-                        </tbody>
-                    </table>
+                        </ul>
+                    </div>
                 </div>
-            </div>
-            {{ Former::number('value')
-                -> readonly()
-                -> disabled()
-                -> placeholder('0.00') }}
-            {{ Asset::push('js','upload')}}
-            <div class="form-group">
-                <label for="dates" class="control-label col-lg-2 col-sm-4">Uploaded</label>
-                <div class="col-lg-10 col-sm-8">
-                    <ul class="list-inline uploaded">
-                        @foreach ($claim->uploads as $file)
-                        <li class="view_uploaded" data-url="{{$file->file_url}}">
-                            <a href="{{$file->file_url}}" target="_blank">{{$file->file_name}}</a>
-                        </li>
-                        @endforeach
-                    </ul>
+                @endif
+                {{ Former::textarea('remarks')
+                    ->readonly()
+                    ->disabled() }}
+                <div class="form-group">
+                    <div class="col-lg-offset-2 col-sm-offset-4 col-lg-10 col-sm-8">
+                        @include('generalclaims.actions-buttons')
+                    </div>
                 </div>
+                {{ Former::close() }}
             </div>
-            {{ Former::textarea('remarks')
-                -> readonly()
-                -> disabled() }}
-            <div class="form-group">
-                <div class="col-lg-offset-2 col-sm-offset-4 col-lg-10 col-sm-8">
-                    @include('generalclaims.actions-buttons')
-                </div>
-            </div>
-            {{ Former::close() }}
         </div>
     </div>
-</div>
+</section>
 @stop
 @section('script')
 @include('generalclaims.actions-scripts')
