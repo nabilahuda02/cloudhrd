3ad4174f790df0b1175f1affc0756f18aa8430d1
Zulfa Juniadi bin Zulkifli <zulfajuniadi@gmail.com>
2016-09-02 12:13:33 +0800

    Fix color picker in types

--- web/app/app/views/leaves/admin/types.blade.php
+++ web/app/app/views/leaves/admin/types.blade.php
@@ -1,82 +1,82 @@
 @extends('layouts.module')
 @section('content')
 <div class="col-md-12">
-
     @include('html.notifications')
-
-    <div class="col-md-12">
-        <div>
-            <div id="leave_type_rows">
-                <div class="page-header">
-                    @include('leaves.menu')
-                    <h3>Leave Types</h3>
+    <div>
+        <div id="leave_type_rows">
+            <div class="page-header">
+                @include('leaves.menu')
+                <h3>Leave Types</h3>
+            </div>
+            <div class="row">
+                <div class="col-md-3">
+                    {{ Former::text('name')
+                    -> label('Leave Type Name')
+                    -> placeholder('Annual Leave') }}
                 </div>
-                <div class="row">
-                    <div class="col-md-3">
-                        {{ Former::text('name')
-                        -> label('Leave Type Name')
-                        -> placeholder('Annual Leave') }}
-                    </div>
-                    <div class="col-md-3">
-                        {{ Former::number('default_entitlement')
-                        -> label('Default Entitlement') }}
-                    </div>
-                    <div class="col-md-3">
-                        {{ Former::text('colors')
-                        -> label('Color')
-                        -> placeholder('#696969')
-                        -> style('color:white') }}
-                    </div>
-                    <div class="col-md-1">
-                        <label for="past">Past</label><br>
-                        <input id="past" type="checkbox" name="past" value="1">
-                    </div>
-                    <div class="col-md-1">
-                        <label for="future">Future</label><br>
-                        <input id="future" type="checkbox" name="future" value="1">
-                    </div>
-                    <!-- <div class="col-md-1">
-                        <label for="display_calendar">Calendar</label><br>
-                        <input id="display_calendar" type="checkbox" name="display_calendar" value="1">
-                    </div> -->
-                    <div class="col-md-1">
-                        <label for="display_wall">Show</label><br>
-                        <input id="display_wall" type="checkbox" name="display_wall" value="1">
-                    </div>
-                    <div class="clearfix"></div>
-                    <br>
+                <div class="col-md-3">
+                    {{ Former::number('default_entitlement')
+                    -> label('Default Entitlement') }}
                 </div>
-                <hr>
+                <div class="col-md-3">
+                    {{ Former::text('colors')
+                    -> label('Color')
+                    -> placeholder('#696969')
+                    -> style('color:white') }}
+                </div>
+                <div class="col-md-1">
+                    <label for="past">Past</label><br>
+                    <input id="past" type="checkbox" name="past" value="1">
+                </div>
+                <div class="col-md-1">
+                    <label for="future">Future</label><br>
+                    <input id="future" type="checkbox" name="future" value="1">
+                </div>
+                <!-- <div class="col-md-1">
+                    <label for="display_calendar">Calendar</label><br>
+                    <input id="display_calendar" type="checkbox" name="display_calendar" value="1">
+                </div> -->
+                <div class="col-md-1">
+                    <label for="display_wall">Show</label><br>
+                    <input id="display_wall" type="checkbox" name="display_wall" value="1">
+                </div>
+                <div class="clearfix"></div>
                 <br>
             </div>
+            <hr>
+            <br>
         </div>
     </div>
-
-    @stop
-    @section('script')
-    <script>
-        $('#leave_type_rows').duplicator({
-            row: ".row",
-            remotes: {
-                post: '/leavetype',
-                put: '/leavetype',
-                delete: '/leavetype',
-                get: '/leavetype'
-            },
-            onAddRow: function(el) {
-                var input = $('[name=colors]', el);
-                var initial = input.val() || '555555';
-                initial = initial.split(',').shift().replace('#', '');
-                input.css('background-color', '#' + initial);
-                input.colpick({
-                    onSubmit: function(hsl, hex) {
-                        $('[name=colors]', el).val('#' + hex);
-                        input.css('background-color', '#' + hex);
-                        input.colpickHide();
-                    }
-                }).colpickSetColor(initial)
+</div>
+@stop
+@section('script')
+<script>
+    $('#leave_type_rows').duplicator({
+        row: ".row",
+        remotes: {
+            post: '/leavetype',
+            put: '/leavetype',
+            delete: '/leavetype',
+            get: '/leavetype'
+        },
+        onAddRow: function(el) {
+            var input = $('[name=colors]', el);
+            var initial = input.val() || '555555';
+            if(initial.indexOf(',') > -1) {
+                initial = initial.split(',').shift();
             }
-        });
-    </script>
+            initial = initial.replace('#', '');
+            input.css('background-color', '#' + initial)
+                .val('#' + initial);
+            input.colpick({
+                onSubmit: function(hsl, hex) {
+                    $('[name=colors]', el).val('#' + hex);
+                    input.css('background-color', '#' + hex);
+                    input.colpickHide();
+                }
+            }).colpickSetColor(initial)
+        }
+    });
+</script>
 
-    @stop
+@stop
--- web/app/app/views/medicals/admin/types.blade.php
+++ web/app/app/views/medicals/admin/types.blade.php
@@ -2,67 +2,68 @@
 @section('content')
 <div class="col-md-12">
     @include('html.notifications')
-    <div class="col-md-12">
-        <div>
-            <div id="medical_type_rows">
-                <div class="page-header">
-                    @include('medicals.menu')
-                    <h3>Medial Claim Types</h3>
+    <div>
+        <div id="medical_type_rows">
+            <div class="page-header">
+                @include('medicals.menu')
+                <h3>Medial Claim Types</h3>
+            </div>
+            <div class="row">
+                <div class="col-md-5">
+                    {{ Former::text('name')
+                    -> label('Entitlement Name')
+                    -> placeholder('Outpatient Claim') }}
                 </div>
-                <div class="row">
-                    <div class="col-md-5">
-                        {{ Former::text('name')
-                        -> label('Entitlement Name')
-                        -> placeholder('Outpatient Claim') }}
-                    </div>
-                    <div class="col-md-3">
-                        {{ Former::number('default_entitlement')
-                        -> label('Default Entitlement') }}
-                    </div>
-                    <div class="col-md-3">
-                        {{ Former::text('colors')
-                        -> placeholder('#696969')
-                        -> label('Color')
-                        -> style('color:white') }}
-                    </div>
-                    <div class="col-md-1">
-                        <label for="display_wall">Wall</label><br>
-                        <input id="display_wall" type="checkbox" name="display_wall" value="1">
-                    </div>
-                    <div class="clearfix"></div>
-                    <br>
+                <div class="col-md-3">
+                    {{ Former::number('default_entitlement')
+                    -> label('Default Entitlement') }}
                 </div>
-                <hr>
+                <div class="col-md-3">
+                    {{ Former::text('colors')
+                    -> placeholder('#696969')
+                    -> label('Color')
+                    -> style('color:white') }}
+                </div>
+                <div class="col-md-1">
+                    <label for="display_wall">Wall</label><br>
+                    <input id="display_wall" type="checkbox" name="display_wall" value="1">
+                </div>
+                <div class="clearfix"></div>
                 <br>
             </div>
+            <hr>
+            <br>
         </div>
     </div>
 </div>
-
 @stop
 @section('script')
-    <script>
-        $('#medical_type_rows').duplicator({
-            row: ".row",
-            remotes: {
-                post: '/medicaltype',
-                put: '/medicaltype',
-                delete: '/medicaltype',
-                get: '/medicaltype'
-            },
-            onAddRow: function(el) {
-                var input = $('[name=colors]', el);
-                var initial = input.val() || '555555';
-                initial = initial.split(',').shift().replace('#', '');
-                input.css('background-color', '#' + initial);
-                input.colpick({
-                    onSubmit: function(hsl, hex) {
-                        $('[name=colors]', el).val('#' + hex);
-                        input.css('background-color', '#' + hex);
-                        input.colpickHide();
-                    }
-                }).colpickSetColor(initial)
+<script>
+    $('#medical_type_rows').duplicator({
+        row: ".row",
+        remotes: {
+            post: '/medicaltype',
+            put: '/medicaltype',
+            delete: '/medicaltype',
+            get: '/medicaltype'
+        },
+        onAddRow: function(el) {
+            var input = $('[name=colors]', el);
+            var initial = input.val() || '555555';
+            if(initial.indexOf(',') > -1) {
+                initial = initial.split(',').shift();
             }
-        });
-    </script>
+            initial = initial.replace('#', '');
+            input.css('background-color', '#' + initial)
+                .val('#' + initial);
+            input.colpick({
+                onSubmit: function(hsl, hex) {
+                    $('[name=colors]', el).val('#' + hex);
+                    input.css('background-color', '#' + hex);
+                    input.colpickHide();
+                }
+            }).colpickSetColor(initial)
+        }
+    });
+</script>
 @stop
