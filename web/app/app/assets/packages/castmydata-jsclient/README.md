ec95153d139fe25237785ea1ad9f7ce171e54e20
Zulfa Juniadi bin Zulkifli <zulfajuniadi@gmail.com>
2016-09-02 11:56:51 +0800

    Removed some packages

--- web/app/app/assets/packages/castmydata-jsclient/README.md
+++
@@ -1,2 +0,0 @@
-# CastMyData Client for Javascript
-
--- web/app/app/assets/packages/castmydata-jsclient/examples/nodejs/index.js
+++
@@ -1,9 +0,0 @@
-var client = require('../../src/castmydata');
-
-var endpoint = new client.Endpoint('https://www.castmydata.com', 'testendpoint');
-
-endpoint.on('broadcast', function(message){
-    console.log(message);
-});
-
-endpoint.broadcast('hey there!');
