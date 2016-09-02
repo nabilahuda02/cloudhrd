34932adc90deb2c2525b9cb4c5c7a46f3550371f
Zulfa Juniadi bin Zulkifli <zulfajuniadi@gmail.com>
2016-09-02 11:45:45 +0800

    Removed unused files

--- web/app/app/assets/javascripts/wall-vendor.js
+++
@@ -1,6 +0,0 @@
-//=require ../packages/uuid-js/lib/uuid.js
-//=require ../packages/async/dist/async.min.js
-//=require ../packages/moment/min/moment.min.js
-//=require ../packages/angular/angular.min.js
-//=require ../packages/angular-moment/angular-moment.min.js
-//=require ../packages/angular-nl2br/angular-nl2br.min.js
