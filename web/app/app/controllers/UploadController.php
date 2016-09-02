28a6fd40208af9007fcd3f5d45834e5fc4cde128
Zulfa Juniadi bin Zulkifli <zulfajuniadi@gmail.com>
2016-09-02 12:15:50 +0800

    Fixed formatting

--- web/app/app/controllers/UploadController.php
+++ web/app/app/controllers/UploadController.php
@@ -3,77 +3,77 @@
 class UploadController extends BaseController
 {
 
-  public function postDo($model, $path, $itemId)
-  {
-    $data = Input::all();
-    $user = Auth::user();
-    $file = Input::file('file');
-    $file_name = $file->getClientOriginalName();
-    $extension = $file->getClientOriginalExtension();
-    $mask = md5(microtime());
-    $new_filename = $mask . '.' . $extension;
-    $thumb_filename = $mask .'__thumb.' . $extension;
+    public function postDo($model, $path, $itemId)
+    {
+        $data = Input::all();
+        $user = Auth::user();
+        $file = Input::file('file');
+        $file_name = $file->getClientOriginalName();
+        $extension = $file->getClientOriginalExtension();
+        $mask = md5(microtime());
+        $new_filename = $mask . '.' . $extension;
+        $thumb_filename = $mask . '__thumb.' . $extension;
 
-    /* create upload directory */
+        /* create upload directory */
 
-    $entriesFolder = public_path() . '/uploads/' . $path . '/';
+        $entriesFolder = public_path() . '/uploads/' . $path . '/';
 
-    if(!is_dir($entriesFolder)) {
-      mkdir($entriesFolder, 0777, true);
+        if (!is_dir($entriesFolder)) {
+            mkdir($entriesFolder, 0777, true);
+        }
+
+        /* insert into database */
+
+        $data = [
+            'mask' => $mask,
+            'file_name' => $file_name,
+            'size' => $file->getSize(),
+            'file_url' => '/uploads/' . $path . '/' . $new_filename,
+            'file_path' => $entriesFolder . $new_filename,
+            'thumb_url' => '/uploads/' . $path . '/' . $thumb_filename,
+            'thumb_path' => $entriesFolder . $thumb_filename,
+        ];
+
+        if (is_numeric($itemId)) {
+            switch ($model) {
+                case 'users':
+                    $item = User::find($itemId);
+                    break;
+                case 'leave':
+                    $item = Leave__Main::find($itemId);
+                    break;
+                case 'medicalclaim':
+                    $item = MedicalClaim__Main::find($itemId);
+                    break;
+                case 'generalclaim':
+                    $item = GeneralClaim__Main::find($itemId);
+                    break;
+            }
+            $entryFile = $item->uploads()->create($data);
+        } else {
+            $data['imageable_type'] = $itemId;
+            $entryFile = Upload::create($data);
+        }
+
+        /* move uploaded file */
+
+        $file->move($entriesFolder, $new_filename);
+
+        Helper::resizeImage2($entryFile->file_path, $entryFile->thumb_path, 200);
+
+        return Response::json($entryFile);
     }
 
-    /* insert into database */
+    public function getRemove($id)
+    {
+        $entryFile = Upload::find($id);
+        if (!$entryFile && !in_array($id, ['Leave__Main', 'MedicalClaim__Main', 'GeneralClaim__Main'])) {
+            $entryFile = Upload::where('mask', $id)->first();
+        }
 
-    $data = [
-      'mask' => $mask,
-      'file_name' => $file_name,
-      'size' => $file->getSize(),
-      'file_url' => '/uploads/' . $path . '/' . $new_filename,
-      'file_path' => $entriesFolder . $new_filename,
-      'thumb_url' => '/uploads/' . $path . '/' . $thumb_filename,
-      'thumb_path' => $entriesFolder . $thumb_filename,
-    ];
-
-    if(is_numeric($itemId)) {
-      switch ($model) {
-        case 'users':
-          $item = User::find($itemId);
-          break;
-        case 'leave':
-          $item = Leave__Main::find($itemId);
-          break;
-        case 'medicalclaim':
-          $item = MedicalClaim__Main::find($itemId);
-          break;
-        case 'generalclaim':
-          $item = GeneralClaim__Main::find($itemId);
-          break;
-      }
-      $entryFile = $item->uploads()->create($data);
-    } else {
-      $data['imageable_type'] = $itemId;
-      $entryFile = Upload::create($data);
+        if (!$entryFile) {
+            return App::abort(404);
+        }
+        $entryFile->delete();
     }
-
-    /* move uploaded file */
-
-    $file->move($entriesFolder, $new_filename);
-
-    Helper::resizeImage2($entryFile->file_path, $entryFile->thumb_path, 200);
-
-    return Response::json($entryFile);
-  }
-
-  public function getRemove($id)
-  {
-    $entryFile = Upload::find($id);
-    if(!$entryFile && !in_array($id, ['Leave__Main', 'MedicalClaim__Main', 'GeneralClaim__Main'])) {
-      $entryFile = Upload::where('mask', $id)->first();
-    }
-
-    if(!$entryFile) {
-      return App::abort(404);
-    }
-    $entryFile->delete();
-  }
 }
--- web/app/app/views/leaves/admin/reporting.blade.php
+++ web/app/app/views/leaves/admin/reporting.blade.php
@@ -1,147 +1,141 @@
 @extends('layouts.module')
 @section('content')
 <div class="col-md-12">
-
-  @include('html.notifications')
-  <div class="col-md-12">
-    <div>
-      <div class="page-header">
-        @include('leaves.menu')
-        <h3>Query Leaves</h3>
-      </div>
-      <div style="padding:15px">
-        {{Former::open_vertical()}}
-          <div class="row">
-            <div class="col-xs-6">
-              {{ Former::select('unit')
-                -> label('Unit')
-                -> selected(@$queryFormData['unit'])
-                -> options(UserUnit::selectOptions()) }}
+    @include('html.notifications')
+    <div class="col-md-12">
+        <div>
+            <div class="page-header">
+                @include('leaves.menu')
+                <h3>Query Leaves</h3>
             </div>
-            <div class="col-xs-6">
-              {{ Former::select('user_id')
-                  -> label('User')
-                  -> selected(@$queryFormData['user_id'])
-                  -> options(User::selectOptions(Leave__Main::$moduleId)) }}
+            {{Former::open_vertical()}}
+            <div class="row">
+                <div class="col-xs-6">
+                    {{ Former::select('unit')
+                        ->label('Unit')
+                        ->selected(@$queryFormData['unit'])
+                        ->options(UserUnit::selectOptions()) }}
+                </div>
+                <div class="col-xs-6">
+                    {{ Former::select('user_id')
+                        ->label('User')
+                        ->selected(@$queryFormData['user_id'])
+                        ->options(User::selectOptions(Leave__Main::$moduleId)) }}
+                </div>
             </div>
-          </div>
-          {{Former::populate(@$queryFormData)}}
-          <div class="row">
-            <div class="col-xs-6">
-              {{ Former::select('status_id')
-                  -> label('Status')
-                  -> options(Status::selectOptions()) }}
+            {{Former::populate(@$queryFormData)}}
+            <div class="row">
+                <div class="col-xs-6">
+                    {{ Former::select('status_id')
+                        ->label('Status')
+                        ->options(Status::selectOptions()) }}
+                </div>
+                <div class="col-xs-6">
+                    {{ Former::select('leave_type_id')
+                        ->label('Type')
+                        ->options(Leave__Type::selectOptions())}}
+                </div>
             </div>
-            <div class="col-xs-6">
-              {{ Former::select('leave_type_id')
-                  -> label('Type')
-                  -> options(Leave__Type::selectOptions())}}
+            <div class="row">
+                <div class="col-xs-12">
+                    <label for="">Leave Taken On:</label>
+                </div>
+                <div class="col-xs-6">
+                    {{ Former::input('leave_from_date')
+                        ->type('date')
+                        ->value(date('Y-m-01'))
+                        ->label('From Date') }}
+                </div>
+                <div class="col-xs-6">
+                    {{ Former::input('leave_to_date')
+                        ->type('date')
+                        ->label('To Date') }}
+                </div>
             </div>
-          </div>
-          <div class="row">
-            <div class="col-xs-12">
-              <label for="">Leave Taken On:</label>
+            <div class="row">
+                <div class="col-xs-12">
+                    <label for="">Leave Created On:</label>
+                </div>
+                <div class="col-xs-6">
+                    {{ Former::input('create_from_date')
+                        ->type('date')
+                        ->label('From Date') }}
+                </div>
+                <div class="col-xs-6">
+                    {{ Former::input('create_to_date')
+                        ->type('date')
+                        ->label('To Date') }}
+                </div>
             </div>
-            <div class="col-xs-6">
-              {{ Former::input('leave_from_date')
-                  -> type('date')
-                  -> value(date('Y-m-01'))
-                  -> label('From Date') }}
+            <div class="row">
+                <div class="col-xs-6">
+                    {{ Former::select('tabulate_by')
+                        ->label('Tabulate By')
+                        ->options([
+                            '' => 'Select One',
+                            'leave_type_id' => 'Leave Types',
+                            'status_id' => 'Status',
+                            'user_id'  => 'User',
+                            'users.unit_id'  => 'Unit',
+                            'year(leave_dates.date), month(leave_dates.date)' => 'Month',
+                        ]) }}
+                </div>
             </div>
-            <div class="col-xs-6">
-              {{ Former::input('leave_to_date')
-                  -> type('date')
-                  -> label('To Date') }}
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-xs-12">
-              <label for="">Leave Created On:</label>
-            </div>
-            <div class="col-xs-6">
-              {{ Former::input('create_from_date')
-                  -> type('date')
-                  -> label('From Date') }}
-            </div>
-            <div class="col-xs-6">
-              {{ Former::input('create_to_date')
-                  -> type('date')
-                  -> label('To Date') }}
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-xs-6">
-              {{ Former::select('tabulate_by')
-                  -> label('Tabulate By')
-                  -> options([
-                      '' => 'Select One',
-                      'leave_type_id' => 'Leave Types',
-                      'status_id' => 'Status',
-                      'user_id'  => 'User',
-                      'users.unit_id'  => 'Unit',
-                      'year(leave_dates.date), month(leave_dates.date)' => 'Month',
-                    ]) }}
-            </div>
-          </div>
-          <hr>
-          <br>
-          <button class="btn btn-primary">Run Query</button>
-          <input class="btn btn-primary" type="submit" name="download" value="Download">
-        {{Former::close()}}
-      </div>
+            <hr>
+            <br>
+            <button type="submit" class="btn btn-primary">Run Query</button>
+            <input class="btn btn-primary" type="submit" name="download" value="Download">
+            {{Former::close()}}
+        </div>
     </div>
-  </div>
-
-  <div class="clearfix"></div>
-  <br>
-
-  @if(isset($tables))
-
-  <div class="col-md-12">
-    <div class="innerLR border-bottom">
-      <div id="leave_type_rows">
-        <h3>
-          Leave Query Results
-        </h3>
-        <hr>
-        @foreach ($tables as $table)
-          <br>
-          <h4>
-            {{$table['title']}}
-          </h4>
-          <br>
-          @include('leaves.admin.reporting-table', ['datas' => $table['data'], 'showaction' => true])
-          <hr>
-          <br>
-        @endforeach
-      </div>
+    <div class="clearfix"></div>
+    <br>
+    @if(isset($tables))
+    <div class="col-md-12">
+        <div class="innerLR border-bottom">
+            <div id="leave_type_rows">
+                <h3>
+                Leave Query Results
+                </h3>
+                <hr>
+                @foreach ($tables as $table)
+                <br>
+                <h4>
+                {{$table['title']}}
+                </h4>
+                <br>
+                @include('leaves.admin.reporting-table', ['datas' => $table['data'], 'showaction' => true])
+                <hr>
+                <br>
+                @endforeach
+            </div>
+        </div>
     </div>
-  </div>
-
-  @endif
+</div>
+@endif
 @stop
 @section('script')
 <script>
-    $('[name="download"]').click(function(e){
-      $('form').attr('target', '_blank');
-      setTimeout(function() {
+$('[name="download"]').click(function(e){
+    $('form').attr('target', '_blank');
+    setTimeout(function() {
         $('form').removeAttr('target');
-      }, 100);
-    });
-    // var table = tbl;
-    // table.columns().eq( 0 ).each( function ( colIdx ) {
-    //   $( 'input[type=text]', table.column(colIdx).footer() ).on('keyup change', function () {
-    //     table
-    //       .column( colIdx )
-    //       .search( this.value )
-    //       .draw();
-    //   });
-    //   $( 'select', table.column(colIdx).footer() ).on('change', function () {
-    //     table
-    //       .column( colIdx )
-    //       .search( this.value )
-    //       .draw();
-    //   });
-    // });
+    }, 100);
+});
+// var table = tbl;
+// table.columns().eq( 0 ).each( function ( colIdx ) {
+//   $( 'input[type=text]', table.column(colIdx).footer() ).on('keyup change', function () {
+//     table
+//       .column( colIdx )
+//       .search( this.value )
+//       .draw();
+//   });
+//   $( 'select', table.column(colIdx).footer() ).on('change', function () {
+//     table
+//       .column( colIdx )
+//       .search( this.value )
+//       .draw();
+//   });
+// });
 </script>
 @stop
